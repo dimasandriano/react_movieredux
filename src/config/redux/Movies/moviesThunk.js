2d41998080ce80b3ce41c202ddf49db19b40f142
dimasandriano <duangdeong@gmail.com>
2023-06-08 14:14:25 +0700

    feat: add project

---
+++ src/config/redux/Movies/moviesThunk.js
@@ -0,0 +1,23 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const apiKey = process.env.REACT_APP_APIKEY;
+const baseUrl = process.env.REACT_APP_BASEURL;
+
+// merupakan fungsi untuk mengambil data dari api. Ini menggunakan axios dan redux thunk untuk menghandle promise.
+export const getMovieList = createAsyncThunk("user/getMovieList", async () => {
+	const movie = await axios.get(
+		`${baseUrl}/movie/popular?page=1&api_key=${apiKey}`
+	);
+	return movie.data.results;
+});
+
+export const searchMovie = createAsyncThunk(
+	"user/searchMovie",
+	async (param) => {
+		const search = await axios.get(
+			`${baseUrl}/search/movie?query=${param}&page=1&api_key=${apiKey}`
+		);
+		return search.data.results;
+	}
+);
